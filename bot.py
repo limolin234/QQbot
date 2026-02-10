aa36a6aa32f7477db1c4df267fb3693956d068ea
limolin234 <limolin234@gmail.com>
2026-02-10 20:15:13 +0800

    gitignore

--- bot.py
+++
@@ -1,9 +0,0 @@
-from ncatbot.core import BotClient
-
-bot = BotClient()
-
-QQnumber = '3603589542'
-
-allowed_id={'1056322921','1075786046'}
-urgent_keywords = {'紧急关键词'}
-normal_keywords = {'一般关键词'}
