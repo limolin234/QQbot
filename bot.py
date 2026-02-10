0297aa25db59dbf09f23a06fd04eb33f083433b2
limolin234 <limolin234@gmail.com>
2026-02-10 20:12:36 +0800

    git ignore updated

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from ncatbot.core import BotClient
 
 bot = BotClient()
 
-QQnumber = '3291053545' #自己的QQ号
+QQnumber = '3603589542'
 
-allowed_id={'1075786046'} #监控的群号
+allowed_id={'1056322921','1075786046'}
 urgent_keywords = {'紧急关键词'}
 normal_keywords = {'一般关键词'}
